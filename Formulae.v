83ffb68389cf20c47558b0901c066260b7589837
Alexandre Moine <alexandre@moine.me>
2020-10-26 14:34:53 +0100

    more

--- Formulae.v
+++ Formulae.v
@@ -25,13 +25,16 @@ Inductive deriv:context -> form -> Prop :=
     deriv G (Or p q) -> (deriv (extend G p) phi) -> (deriv (extend G q) phi) -> deriv G phi
 | ImplI : forall (G:context) p q, deriv (extend G p) q -> deriv G (Impl p q)
 | ImplE : forall (G:context) p q, deriv G (Impl p q) -> deriv G p -> deriv G q
-| AllI : forall A (G:context) p, (forall a, deriv G (p a)) -> deriv G (@All A p)
-| AllE : forall A (G:context) p a, deriv G (@All A p) -> deriv G (p a)
-| ExI  : forall A (G:context) p a, deriv G (p a) -> deriv G (@Ex A p)
-| ExE  : forall A (G:context) p q, deriv G (@Ex A p) -> (forall a, deriv (extend G (p a)) q) -> deriv G q.
+| AllI : forall (G:context) A p, (forall a, deriv G (p a)) -> deriv G (@All A p)
+| AllE : forall (G:context) A p, deriv G (@All A p) -> forall a, deriv G (p a)
+| ExI  : forall (G:context) A p a, deriv G (p a) -> deriv G (@Ex A p)
+| ExE  : forall (G:context) A p q, deriv G (@Ex A p) -> (forall a, deriv (extend G (p a)) q) -> deriv G q.
 
 Create HintDb derivdb.
 Hint Constructors deriv : derivdb.
+
+Ltac explore_context := repeat ((left; easy) + right).
+Ltac axiom := apply Ax; explore_context.
 
 Definition classical := fun hyp => exists A:form, hyp = Or A (Impl A Fa).
 
@@ -45,17 +48,15 @@ Proof.
   apply OrE with (p:=(All (fun p => Atom (drinks p)))) (q:= neg (All (fun p => Atom (drinks p)))).
   - apply Ax. exists ((All (fun p => Atom (drinks p)))); easy.
   - apply ExI with (a:=barfly).
-    apply ImplI, Ax.
-    right; left.
-    easy.
+    apply ImplI; axiom.
   - apply ImplE with (p:=Ex (fun x => neg (Atom (drinks x)))). (* There exists someone not drinking *)
     + apply ImplI.
       apply ExE with _ (fun x : bar => neg (Atom (drinks x))).
-      * apply Ax; left; easy.
+      * axiom.
       * intros a.
         apply ExI with a.
         apply ImplI, FaE.
-        apply ImplE with (p:=(Atom (drinks a))); apply Ax; [right; left | left] ; easy.
+        apply ImplE with (p:=(Atom (drinks a))); axiom.
     + admit.
 Admitted.
 
@@ -118,31 +119,69 @@ Fixpoint nnt (x:form) :=
   | All p => All (fun x => nnt (p x))
   | Ex p => neg (neg (Ex (fun x => nnt (p x)))) end.
 
+Lemma extend_context L p f : deriv L p -> deriv (extend L p) f -> deriv L f.
+Proof.
+  intros H x.
+  apply deriv_substitution with (L:= extend L p).
+  - easy.
+  - intros y I.
+    destruct I.
+    + now destruct H0.
+    + eauto with derivdb.
+Qed.
+
+(* Apply an arrow that is in the context *)
+Ltac harrow p := apply ImplE with p; only 1:axiom.
+
 Lemma double_elimination L f : deriv L (neg (neg (nnt f))) -> deriv L (nnt f).
 Proof.
   revert L; induction f; intros L D; simpl in *.
   - eauto with derivdb.
-  - apply deriv_substitution with (L:= extend L (neg (neg Fa))).
-    + apply ImplE with (p:=neg Fa).
-      * apply Ax; left; easy.
-      * apply ImplI, Ax; left; easy.
-    + intros f E.
-      destruct E.
-      * now destruct H.
-      * now apply Ax.
+  - apply extend_context with (p:=neg (neg Fa)); try easy.
+    harrow (neg Fa).
+    apply ImplI; axiom.
+  - apply extend_context with (p:=(neg (neg (And (nnt f1) (nnt f2))))); try easy.
+    apply AndI.
+    + apply IHf1, ImplI.
+      harrow (neg (And (nnt f1) (nnt f2))).
+      apply ImplI.
+      harrow (nnt f1).
+      apply AndEL with (q:= nnt f2); axiom.
+    + apply IHf2, ImplI.
+      harrow (neg (And (nnt f1) (nnt f2))).
+      apply ImplI.
+      harrow (nnt f2).
+      apply AndER with (p:= nnt f1); axiom.
   - admit.
+  - apply extend_context with (p:=(neg (neg (Impl (nnt f1) (nnt f2))))); try easy.
+    apply ImplI, IHf2, ImplI.
+    harrow (neg (Impl (nnt f1) (nnt f2))).
+    apply ImplI.
+    harrow (nnt f2); harrow (nnt f1); axiom.
+  - apply extend_context with (p:=(neg (neg (All (fun x : A => nnt (f x)))))); try easy.
+    apply AllI; intros a.
+    apply H.
+    apply ImplI.
+    harrow (neg (All (fun x : A => nnt (f x)))).
+    apply ImplI.
+    harrow (nnt (f a)).
+    admit.
   - admit.
-  - admit.
-  - admit.
-  - admit.
-  - admit.
+  - apply extend_context with (neg (neg (neg (neg (Atom P))))); try easy.
+    apply ImplI.
+    harrow (neg (neg (neg (Atom P)))).
+    apply ImplI.
+    harrow (neg (Atom P)).
+    apply ImplI.
+    harrow (Atom P).
+    axiom.
 Admitted.
 
 Definition nnt_context L := fun p => exists p', p=nnt p' /\ L p'.
 
 Lemma ntt_soundness L f : deriv L f -> deriv (nnt_context L) (nnt f).
 Proof.
-  induction 1.
+  induction 1; simpl in *.
   - eauto with derivdb.
   - eauto with derivdb.
   - apply Ax; exists x; easy.
@@ -159,3 +198,17 @@ Proof.
   - admit.
   - admit.
 Admitted.
+
+Inductive classic : form -> Prop :=
+  Cem P : classic (Or P (Impl P Fa)).
+
+Definition union (L L' : context) := fun y => L y \/ L' y.
+
+Lemma excluded_middle_elim L f : deriv (union classic L) f -> deriv (nnt_context L) (nnt f).
+Proof.
+Admitted.
+
+Inductive equality (A:Type) : form -> Prop :=
+| Reflexivity : equality A (All (fun x:A => Atom (x=x)))
+| Symmetry : equality A (All (fun x:A => All (fun y => Impl (Atom (x=y)) (Atom (y=x)))))
+| Transitivity : equality A (All (fun x:A =>All (fun y => All (fun z => Impl (Atom (x=y)) (Impl (Atom (y=z)) (Atom (x=z))))) )).
